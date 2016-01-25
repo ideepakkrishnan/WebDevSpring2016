58101ba3f5192b48e3f3daec02a230d3cca266c5
deepakkrishnan <krishnan.d@husky.neu.edu>
2016-01-24 22:39:44 -0500

    Adding modals inside form-fields

---
+++ public/assignment/js/forms.js
@@ -0,0 +1,11 @@
+/**
+ * Created by ideepakkrishnan on 24-01-2016.
+ */
+
+/*
+ * Overloading default tab behaviors:
+ * Adding jQuery code to load HTML pages into a Bootstrap tab. The default divs inside forms.html are considered as
+ * placeholders and on clicking the fields tab, the corresponding page which  contains the data is loaded
+ */
+
+$('#fields').load('form-fields.html');
