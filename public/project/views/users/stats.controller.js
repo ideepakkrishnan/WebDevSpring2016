8e42bbbc8e50960d26adf06b903ba52f38ae770d
deepakkrishnan <krishnan.d@husky.neu.edu>
2016-03-04 10:54:50 -0500

    Updating stats and team pages along with minor changes in dashboard, register and sidebar pages

---
+++ public/project/views/users/stats.controller.js
@@ -0,0 +1,79 @@
+/**
+ * Created by ideepakkrishnan on 04-03-2016.
+ */
+
+(function () {
+    angular
+        .module("PerformXApp")
+        .controller("StatsController", statsController);
+
+    function statsController($scope, $rootScope, $location, UserService) {
+        if ($rootScope.currentUser) {
+            $scope.userId = $rootScope.currentUser._id;
+            $scope.username = $rootScope.currentUser.username;
+            $scope.password = $rootScope.currentUser.password;
+            $scope.firstName = $rootScope.currentUser.firstName;
+            $scope.lastName = $rootScope.currentUser.lastName;
+            $scope.userEmail = $rootScope.currentUser.email;
+            $scope.teams = $rootScope.currentUser.teams;
+            $scope.roles = $rootScope.currentUser.roles;
+            UserService.fetchTeamDetails(
+                $scope.teams,
+                function(response) {
+                    console.log(response);
+                    $scope.myTeams = response;
+                }
+            );
+
+            var div1=d3.select(document.getElementById('div1'));
+            var div2=d3.select(document.getElementById('div2'));
+            var div3=d3.select(document.getElementById('div3'));
+            var div4=d3.select(document.getElementById('div4'));
+
+            start();
+        } else {
+            $location.path("#/home");
+        }
+
+        function onClick1() {
+
+        }
+
+        function onClick2() {
+
+        }
+
+        function onClick3() {
+
+        }
+
+        function labelFunction(val,min,max) {
+
+        }
+
+        function start() {
+            var rp1 = radialProgress(document.getElementById('div1'))
+                .label("Sleep")
+                .onClick(onClick1)
+                .diameter(150)
+                .value(78)
+                .render();
+
+            var rp2 = radialProgress(document.getElementById('div2'))
+                .label("Activity")
+                .onClick(onClick2)
+                .diameter(150)
+                .value(132)
+                .render();
+
+            var rp3 = radialProgress(document.getElementById('div3'))
+                .label("Weight")
+                .onClick(onClick3)
+                .diameter(150)
+                .minValue(100)
+                .maxValue(200)
+                .value(150)
+                .render();
+        }
+    }
+})();
