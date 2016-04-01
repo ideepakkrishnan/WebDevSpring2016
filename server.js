8a2180dbeafd3ba1f991fe7aaa1d2e5f06e82ff8
deepakkrishnan <krishnan.d@husky.neu.edu>
2016-04-01 19:15:00 -0400

    Temporary changes to track openshift issue - 2

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
         process.env.OPENSHIFT_APP_NAME;
 }
 
+console.log("Env variable for db host: " + process.env.OPENSHIFT_MONGODB_DB_HOST);
 console.log("Connection string: " + connectionString);
 
 // connect to the database
